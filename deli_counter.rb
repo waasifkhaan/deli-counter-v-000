90dd9ae24c37545190426355791857cd556f27dd
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 17:44:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
 end
 def take_a_number(katz_deli,name)
   katz_deli << name
-  "Welcome #[name}. You are number #{katz_deli.length} in line."
+  "Welcome #{name}. You are number #{katz_deli.length} in line."
   end
 def now_serving 
   if katz_deli = ""
