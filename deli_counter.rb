d20534a3dfb2ce137a747bf2019611a47ae0862b
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 18:12:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 Write your code here.
-katz_deli = []
-def line(katz_deli) 
-  if katz_deli == ""
-    "The line is currently empty."
-  else
-    katz_deli.map_with_index do |cus,index|
-      puts "The line is currently #{index + 1}. #{cus}"
-        end
-  end
-end
+# katz_deli = []
+# def line(katz_deli) 
+#   if katz_deli == ""
+#     "The line is currently empty."
+#   else
+#     katz_deli.map_with_index do |cus,index|
+#       puts "The line is currently #{index + 1}. #{cus}"
+#         end
+#   end
+# end
 def take_a_number(katz_deli,name)
   katz_deli << name
   "Welcome #{name}. You are number #{katz_deli.length} in line."
