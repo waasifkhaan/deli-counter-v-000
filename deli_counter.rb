96bac02625c4bece688d33e6cd750410a86b19e8
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 17:35:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,16 @@ def line
     "The line is currently empty."
   else
     "The line is currently:#{katz_deli.map.with_index do |cus,index|
-                    katz_deli[index+1]. cus"
+                    katz_deli[index+1]. cus}"
                   end
-  end
+    end
 end
 def take_a_number(katz_deli,name)
   katz_deli << name
-  puts The line is currently:#{katz_deli.map.with_index do |cus,index|
-                    katz_deli[index+1]. cus"
+  puts "The line is currently:#{katz_deli.map.with_index do |cus,index|
+                    katz_deli[index+1]. cus}"
                   end
   end
 def now_serving 
-  puts "#{katz_deli[]"
+    puts "#{katz_deli.unshift}"
+end
