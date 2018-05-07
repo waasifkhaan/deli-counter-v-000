0ff8dfa505afe5417d7dafcad5b6145b993db98f
Wasif Khan <waasif.khaan@gmail.com>
2018-05-07 18:14:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 def line(katz_deli) 
   if katz_deli == ""
-    "The line is currently empty."
+    "The line is empty."
   else
     katz_deli.map_with_index do |cus,index|
       puts "The line is currently #{index + 1}. #{cus}"
